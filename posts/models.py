7c5763e934dc76eae3ab2b20d40716ba0a75012a
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-03 14:19:09 -0500

    added model

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    post_title = models.CharField(max_length = 100)
+    post_text = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    post_author = models.ForeignKey(User, on_delete =models.CASCADE)
+
