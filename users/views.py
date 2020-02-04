4df91debd1cfadff5cba06369a85001ea9e04f08
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-04 16:11:23 -0500

    added login

---
+++ users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .forms import RegistrationForm
+
+def register(request):
+    if request.method =='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = RegistrationForm()
+
+    context = {'form':form}
+    return render(request, 'users/register.html', context)
