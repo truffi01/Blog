a91fa9bc36ab55eb2928c8ccaaf911373b4e950e
Thomas Ruffino <thomasruffino1@gmail.com>
2020-02-04 16:34:29 -0500

    finished

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class HomePage(ListView):
+class HomePage(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'posts/index.html'
     context_object_name = "blog_posts"
     ordering = ['-post_date']
     paginate_by = 3
 
-class PostView(DetailView):
+class PostView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'posts/post_detail.html'
 
-class CreatePostView(CreateView):
+class CreatePostView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'posts/create_post.html'
     fields = ['post_title', 'post_text']
